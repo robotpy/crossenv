a481b90f611585dee3eb7632924f681f1d755126
Dustin Spicuzza <dustin@virtualroadside.com>
2025-09-13 01:20:45 -0400

    Format code with black, add CI

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -10,37 +10,60 @@ from .testutils import make_crossenv
 
 
 def test_set_c_compiler(tmp_path, host_python, build_python):
-    crossenv = make_crossenv(tmp_path, host_python, build_python,
-            '--cc=/bin/true')
-    compiler = crossenv.check_output(['python', '-c', dedent('''\
+    crossenv = make_crossenv(tmp_path, host_python, build_python, "--cc=/bin/true")
+    compiler = crossenv.check_output(
+        [
+            "python",
+            "-c",
+            dedent(
+                """\
         import sysconfig
         print(sysconfig.get_config_var("CC"))
-        ''')])
+        """
+            ),
+        ]
+    )
     compiler = compiler.split()[0]
-    assert compiler == b'/bin/true'
+    assert compiler == b"/bin/true"
+
 
 def test_set_cxx_compiler(tmp_path, host_python, build_python):
-    crossenv = make_crossenv(tmp_path, host_python, build_python,
-            '--cxx=/bin/true')
-    compiler = crossenv.check_output(['python', '-c', dedent('''\
+    crossenv = make_crossenv(tmp_path, host_python, build_python, "--cxx=/bin/true")
+    compiler = crossenv.check_output(
+        [
+            "python",
+            "-c",
+            dedent(
+                """\
         import sysconfig
         print(sysconfig.get_config_var("CXX"))
-        ''')])
+        """
+            ),
+        ]
+    )
     compiler = compiler.split()[0]
-    assert compiler == b'/bin/true'
+    assert compiler == b"/bin/true"
+
 
 def test_set_ar(tmp_path, host_python, build_python):
-    crossenv = make_crossenv(tmp_path, host_python, build_python,
-            '--ar=/bin/true')
-    compiler = crossenv.check_output(['python', '-c', dedent('''\
+    crossenv = make_crossenv(tmp_path, host_python, build_python, "--ar=/bin/true")
+    compiler = crossenv.check_output(
+        [
+            "python",
+            "-c",
+            dedent(
+                """\
         import sysconfig
         print(sysconfig.get_config_var("AR"))
-        ''')])
+        """
+            ),
+        ]
+    )
     compiler = compiler.split()[0]
-    assert compiler == b'/bin/true'
+    assert compiler == b"/bin/true"
 
-def test_wrong_architecture(tmp_path, host_python, build_python,
-        architecture):
+
+def test_wrong_architecture(tmp_path, host_python, build_python, architecture):
     """Make sure we get a warning if the compiler doesn't seem right. Requires
     gcc."""
 
@@ -48,9 +71,8 @@ def test_wrong_architecture(tmp_path, host_python, build_python,
     if architecture.machine == platform.machine():
         return
 
-    crossenv = make_crossenv(tmp_path, host_python, build_python,
-            '--cc=/usr/bin/gcc')
+    crossenv = make_crossenv(tmp_path, host_python, build_python, "--cc=/usr/bin/gcc")
     for line in crossenv.creation_log.splitlines():
-        if re.match(r'WARNING:.*architecture', line):
+        if re.match(r"WARNING:.*architecture", line):
             return
     assert False, "Crossenv did not detect wrong architecture"
--- tests/testutils.py
+++ tests/testutils.py
@@ -5,9 +5,10 @@ import string
 import fcntl
 from contextlib import contextmanager
 
+
 def hash_file(path):
     ctx = hashlib.sha256()
-    with open(path, 'rb') as fp:
+    with open(path, "rb") as fp:
         data = fp.read(0x4000)
         while data:
             ctx.update(data)
@@ -16,12 +17,12 @@ def hash_file(path):
 
 
 class ExecEnvironment:
-    '''Mostly some wrappers around popen'''
+    """Mostly some wrappers around popen"""
 
     def __init__(self):
         # prepopulate the environment
         self.environ = {
-            'PATH': os.environ.get('PATH', ''),
+            "PATH": os.environ.get("PATH", ""),
         }
         self.cwd = None
 
@@ -30,11 +31,13 @@ class ExecEnvironment:
         # environment. We might clobber changes to $PATH, etc. if we use
         # it.
         class EnvDict:
-            '''trick Template.substitute into converting $NOTFOUND into '''
+            """trick Template.substitute into converting $NOTFOUND into"""
+
             def __init__(self, src):
                 self.src = src
+
             def __getitem__(self, key):
-                return self.src.get(key, '')
+                return self.src.get(key, "")
 
         if value is None:
             return value
@@ -68,33 +71,29 @@ class ExecEnvironment:
 
     def _popen(self, func, *args, **kwargs):
         if self.environ:
-            env = kwargs.get('env', os.environ)
+            env = kwargs.get("env", os.environ)
             env = self._alter_env(self.environ, env)
-            kwargs['env'] = env
-        if self.cwd and 'cwd' not in kwargs:
-            kwargs['cwd'] = self.cwd
+            kwargs["env"] = env
+        if self.cwd and "cwd" not in kwargs:
+            kwargs["cwd"] = self.cwd
 
         return func(*args, **kwargs)
-
 
     def run(self, *args, **kwargs):
         return self._popen(subprocess.run, *args, **kwargs)
 
-
     def popen(self, *args, **kwargs):
         return self._popen(subprocess.Popen, *args, **kwargs)
 
-
     def check_call(self, *args, **kwargs):
         return self._popen(subprocess.check_call, *args, **kwargs)
-
 
     def check_output(self, *args, **kwargs):
         return self._popen(subprocess.check_output, *args, **kwargs)
 
 
 class CrossenvEnvironment(ExecEnvironment):
-    def __init__(self, build_python, crossenv_dir, creation_log=''):
+    def __init__(self, build_python, crossenv_dir, creation_log=""):
         super().__init__()
 
         self.creation_log = creation_log
@@ -102,43 +101,45 @@ class CrossenvEnvironment(ExecEnvironment):
         self.cwd = build_python.cwd
 
         self.crossenv_dir = crossenv_dir
-        self.bindir = crossenv_dir / 'bin'
+        self.bindir = crossenv_dir / "bin"
 
-        site = sorted(crossenv_dir.glob('build/lib/python*/site-packages'))[0]
-        self.build_bindir = crossenv_dir / 'build/bin'
+        site = sorted(crossenv_dir.glob("build/lib/python*/site-packages"))[0]
+        self.build_bindir = crossenv_dir / "build/bin"
         self.build_site_packages = site
 
-        sites = crossenv_dir.glob('cross/lib/python*/site-packages')
+        sites = crossenv_dir.glob("cross/lib/python*/site-packages")
         sites = sorted(sites)
         if sites:
             self.cross_site_packages = sites[0]
-            self.cross_bindir = crossenv_dir / 'cross/bin'
+            self.cross_bindir = crossenv_dir / "cross/bin"
         else:
             self.cross_site_packages = None
             self.cross_bindir = None
 
         # Mimic some of what crossenv_dir/bin/activate would do
-        self.setenv('PATH', '{}:{}:$PATH'.format(self.bindir,
-            self.cross_bindir))
+        self.setenv("PATH", "{}:{}:$PATH".format(self.bindir, self.cross_bindir))
+
 
 def make_crossenv(crossenv_dir, host_python, build_python, *args, **kwargs):
-    cmdline = [ build_python.binary, '-m', 'crossenv', host_python.binary,
-            crossenv_dir ]
+    cmdline = [build_python.binary, "-m", "crossenv", host_python.binary, crossenv_dir]
     cmdline.extend(args)
-    result = build_python.run(cmdline,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT, universal_newlines=True,
-            **kwargs)
+    result = build_python.run(
+        cmdline,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+        **kwargs,
+    )
     if result.returncode:
-        print(result.stdout) # capture output on error
+        print(result.stdout)  # capture output on error
         assert False, "Could not make crossenv!"
-    return CrossenvEnvironment(build_python, crossenv_dir,
-            creation_log=result.stdout)
+    return CrossenvEnvironment(build_python, crossenv_dir, creation_log=result.stdout)
+
 
 @contextmanager
 def open_lock_file(path):
     fd = os.open(path, os.O_RDWR | os.O_CREAT, 0o660)
-    with open(fd, 'r+') as fp:
+    with open(fd, "r+") as fp:
         fcntl.flock(fp, fcntl.LOCK_EX)
         try:
             data = fp.read(64)
